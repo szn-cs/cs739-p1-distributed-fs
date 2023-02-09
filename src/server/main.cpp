1aa0f732ec767d6801092f8162efa1452675db3e
uycuhnt2467 <uycuhnt2467@gmail.com>
2023-02-08 20:08:43 -0600

    Apply minimum server

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,5 +1,80 @@
-#include <iostream>
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+// todo
+
+#include <iostream>
+#include <sys/stat.h>
+#include <memory>
+#include <string>
+
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+#include "afs.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using afs::CustomAFS;
+using afs::Path;
+using afs::Reply;
+// Logic and data behind the server's behavior.
+class AFSServerServiceImpl final : public CustomAFS::Service {
+  Status Mkdir(ServerContext* context, const Path* request,
+                  Reply* response) override {
+    // response->set_return_code(0);
+    // if(mkdir(getServerFilepath(request->filepath()).c_str(), 00777) == -1) {
+    //     response->set_return_code(-1);
+    //     response->set_error_number(errno);
+    // }
+    
+    
+    response->set_status(1);
+    return Status::OK;
+  }
+};
+
+void RunServer() {
+  std::string server_address("0.0.0.0:50051");
+  AFSServerServiceImpl service;
+
+  grpc::EnableDefaultHealthCheckService(true);
+  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+  ServerBuilder builder;
+  // Listen on the given address without any authentication mechanism.
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  // Register "service" as the instance through which we'll communicate with
+  // clients. In this case it corresponds to an *synchronous* service.
+  builder.RegisterService(&service);
+  // Finally assemble the server.
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+
+  // Wait for the server to shutdown. Note that some other thread must be
+  // responsible for shutting down the server for this call to ever return.
+  server->Wait();
+}
+
+int main(int argc, char** argv) {
+    
+  RunServer();
+
+  return 0;
 }
