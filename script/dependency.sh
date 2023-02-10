133355f90c18f646ead6e330022018893dc205e7
safi <nassarsafi@gmail.com>
2023-02-09 22:32:14 -0500

    add Fedora instructions

--- script/dependency.sh
+++ script/dependency.sh
@@ -1,7 +1,7 @@
 # provision system dependencies
 # - libfuse 3+
 
-dependencies="build-essential pkg-config cmake fuse3 libfuse3-dev"
+dependencies="build-essential pkg-config gcc cmake fuse3 libfuse3-dev"
 
 apt update && apt upgrade
 for i in $dependencies; do apt install -y $i; done
@@ -9,3 +9,8 @@ apt update && apt upgrade
 apt autoremove
 
 exit 0
+
+# Fedora gcc installation
+# dnf groupinstall 'Development Tools'
+# yum install gcc-c++
+# yum install fuse fuse-devel
